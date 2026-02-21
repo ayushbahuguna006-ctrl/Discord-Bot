1855e4e673fe0c8e1bec8391932893c5cbcc3eb6
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-02-21 12:40:23 +0530

    project completed

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+client.on('messageCreate',(message)=>{
+    if (message.author.bot) return;
+    if(message.content.startsWith('create')){
+        const url=message.content.split('create')[1]
+        return message.reply({
+            content:'Generating short-id for'+url
+        })
+    }
+    message.reply({
+        content:'Hi Ayush ! How can I help you?'
+    })
+})
+client.login(process.env.DISCORD_TOKEN);
